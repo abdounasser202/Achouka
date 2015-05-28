4296459baf69925ae649fd3921484be691bb7088
wilrona <wilrona@gmail.com>
2015-05-28 15:16:34 +0100

    init commit 2

--- application/modules/profil/models_profil.py
+++ application/modules/profil/models_profil.py
@@ -2,8 +2,8 @@ __author__ = 'wilrona'
 
 from google.appengine.ext import ndb
 
+
 class ProfilModel(ndb.Model):
     name = ndb.StringProperty()
     standard = ndb.BooleanProperty(default=False)
     enable = ndb.BooleanProperty(default=True)
-
