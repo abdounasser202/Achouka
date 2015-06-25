1e632459e8ac8dbcdc0a4a93fe197b705d1d46e0
nasserzone <abdounasser202@gmail.com>
2015-06-25 13:00:37 +0100

    25/06/2015 commit 2

--- application/modules/customer/models_customer.py
+++ application/modules/customer/models_customer.py
@@ -7,6 +7,8 @@ class CustomerModel(ndb.Model):
     first_name = ndb.StringProperty()
     last_name = ndb.StringProperty()
     birthday = ndb.DateProperty()
+    passport_number = ndb.IntegerProperty()
+    nic_number = ndb.IntegerProperty()
     profession = ndb.StringProperty()
     nationality = ndb.StringProperty()
     phone = ndb.StringProperty()
